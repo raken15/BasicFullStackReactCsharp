81d1be16bea48df0e287950126fcf7936b4b03bd
Raviv <raken100@gmail.com>
2025-02-28 12:56:09 +0200

    added more code to allow cors for my frontend part later

--- BackendCsharp/Program.cs
+++ BackendCsharp/Program.cs
@@ -4,6 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+// Configure CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // Adjust to your frontend URL
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
