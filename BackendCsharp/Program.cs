31b912961e73f0b5499a892be6e1d2690fe99564
Raviv <raken100@gmail.com>
2025-03-07 14:38:52 +0200

    added the message service and interface as singelton to be injected and used by the message controller

--- BackendCsharp/Program.cs
+++ BackendCsharp/Program.cs
@@ -1,7 +1,10 @@
+using BackendCsharp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IMessagesService, MessagesService>();
 builder.Services.AddSwaggerGen();
 
 // Configure CORS policy
