87023d09a48f0ae446c9885049f7bb5db33d2f76
Raviv <raken100@gmail.com>
2025-03-07 14:39:23 +0200

    Implemented all the http methods using the new singleton message service and interface

--- BackendCsharp/Controllers/MessagesController.cs
+++ BackendCsharp/Controllers/MessagesController.cs
@@ -1,3 +1,5 @@
+using BackendCsharp.Services;
+using BackendCsharp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackendCsharp.Controllers;
@@ -6,33 +8,38 @@ namespace BackendCsharp.Controllers;
 [ApiController]
 public class MessagesController : ControllerBase
 {
-    private List<string> _messages = new List<string> { "Hello from C#", "Welcome to React and Csharp" };
+    private readonly IMessagesService _messages;
+    public MessagesController(IMessagesService messages)
+    {
+        _messages = messages;
+    }
+    // private List<string> _messages = new List<string> { "Hello from C#", "Welcome to React and Csharp" };
     [HttpGet]
     public IActionResult GetAllMessages()
     {
-        return Ok(_messages);
+        return Ok(_messages.GetAllMessages());
     }
     [HttpGet("{id}")]
     public IActionResult GetMessage(int id)
     {
-        return Ok(_messages[id]);
+        return Ok(_messages.GetMessage(id));
     }
     [HttpPost]
     public IActionResult PostMessage([FromBody] string message)
     {
-        _messages.Add(message);
+        _messages.AddMessage(new Message { Text = message, CreatedAt = DateTime.Now });
         return CreatedAtAction(nameof(GetMessage), new { id = _messages.Count - 1 }, message);
     }
     [HttpPut("{id}")]
-    public IActionResult PutMessage(int id, [FromBody] string message)
+    public IActionResult PutMessage(int id, [FromBody] Message message)
     {
-        _messages[id] = message;
+        _messages.UpdateMessage(id, message);
         return NoContent();
     }
     [HttpDelete("{id}")]
     public IActionResult DeleteMessage(int id)
     {
-        _messages.RemoveAt(id);
+        _messages.DeleteMessage(id);
         return NoContent();
     }
 }
