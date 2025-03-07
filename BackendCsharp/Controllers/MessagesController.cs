fa51ccea52c69614a434db3b579f0ee6ff77e927
Raviv <raken100@gmail.com>
2025-03-07 14:22:43 +0200

    improved the controller and implemented the rest of the crud Http methods

--- BackendCsharp/Controllers/MessagesController.cs
+++ BackendCsharp/Controllers/MessagesController.cs
@@ -6,11 +6,33 @@ namespace BackendCsharp.Controllers;
 [ApiController]
 public class MessagesController : ControllerBase
 {
+    private List<string> _messages = new List<string> { "Hello from C#", "Welcome to React and Csharp" };
     [HttpGet]
     public IActionResult GetAllMessages()
     {
-        var messages = new List<string> { "Hello from C#", "Welcome to React and Csharp"};
-        return Ok(messages);
+        return Ok(_messages);
     }
-
+    [HttpGet("{id}")]
+    public IActionResult GetMessage(int id)
+    {
+        return Ok(_messages[id]);
+    }
+    [HttpPost]
+    public IActionResult PostMessage([FromBody] string message)
+    {
+        _messages.Add(message);
+        return CreatedAtAction(nameof(GetMessage), new { id = _messages.Count - 1 }, message);
+    }
+    [HttpPut("{id}")]
+    public IActionResult PutMessage(int id, [FromBody] string message)
+    {
+        _messages[id] = message;
+        return NoContent();
+    }
+    [HttpDelete("{id}")]
+    public IActionResult DeleteMessage(int id)
+    {
+        _messages.RemoveAt(id);
+        return NoContent();
+    }
 }
